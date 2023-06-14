71a3cbcf430bab193b700d944f76b069654ed7fd
Pratik Thapa <pratik@tldrnotes.net>
2023-06-14 09:13:22 -0600

    singly linked list complete

--- algorithms/4-linkedList.go
+++ algorithms/4-linkedList.go
@@ -32,14 +32,42 @@ func (l linkedList) printLinkedListData() {
 	fmt.Println("\n")
 }
 
+// Delete value from the node of a given linkedlist
+func (l *linkedList) deleteNodeValue(value int) {
+	if l.length == 0 {
+		return // return if there is no linked list
+	}
+	if l.head.data == value {
+		// if we are trying to delete the header, we need to update linked list with a new header
+		l.head = l.head.next
+		l.length--
+		return
+	}
+	toDelete := l.head                // start at thead.
+	for toDelete.next.data != value { // assign valueToDelete new next data until, we reach the value
+		if toDelete.next.next == nil {
+			return // return if the value does not exists in linked list
+		}
+		toDelete = toDelete.next // once the value is found, assign the value to delete
+	}
+	toDelete.next = toDelete.next.next // once the value is found, set the pointer to next node
+	l.length--                         // decrease the length once done.
+
+}
+
 func LinkedList() {
 	node1 := &node{data: 200}
 	node2 := &node{data: 20}
 	node3 := &node{data: 2}
+	node4 := &node{data: 1}
 	newLinkedList := linkedList{}
 	newLinkedList.prepend(node1)
 	newLinkedList.prepend(node2)
 	newLinkedList.prepend(node3)
-	fmt.Println(newLinkedList) // Expected Result {memoryAddress numberOfitems}. {0x140000102b0 3} in my case
-	newLinkedList.printLinkedListData()
+	newLinkedList.prepend(node4)
+	fmt.Println(newLinkedList)          // Expected Result {memoryAddress numberOfitems}. {0x140000102b0 3} in my case
+	newLinkedList.printLinkedListData() // Expected Result: 1, 2, 20, 200
+	newLinkedList.deleteNodeValue(1)    // delete function
+	newLinkedList.printLinkedListData() // Expected Result:1, 2, 20
+
 }
