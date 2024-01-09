37beba802dfc31b1da44ec7a2f494f7b549bf84e
Pratik Thapa <pratik@tldrnotes.net>
2024-01-08 22:16:16 -0500

    minor ...

--- problems/strings.go
+++ problems/strings.go
@@ -7,16 +7,11 @@ import (
 // given a string, reverse it's character.
 func ReverseString(s string) string {
 	// hello should be olleh
-
 	if len(s) <= 1 {
 		return s
 	}
 	array := strings.Split(s, "")
-
-	lastIndex := len(array) - 1
-	lastCharacter := array[lastIndex]
-
-	restReversed := ReverseString(s[:len(s)-1])
-
-	return lastCharacter + restReversed
+	lastCharacter := array[len(array)-1]
+	reverseRest := ReverseString(s[:len(s)-1])
+	return lastCharacter + reverseRest
 }
